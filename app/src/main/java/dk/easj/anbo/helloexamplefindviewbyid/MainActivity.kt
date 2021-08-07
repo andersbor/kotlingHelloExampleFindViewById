827a132b10c6d86555baac7bb9acbdf725b203bb
Anders Børjesson <anbo@zealand.dk>
2021-08-07 10:19:15 +0200

    minor updates

--- app/src/main/java/dk/easj/anbo/helloexamplefindviewbyid/MainActivity.kt
+++ app/src/main/java/dk/easj/anbo/helloexamplefindviewbyid/MainActivity.kt
@@ -1,5 +1,6 @@
 package dk.easj.anbo.helloexamplefindviewbyid
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
